28708e1cda178ac03d107c513eeeac172d3dd4db
Daniele <danileaodev@gmail.com>
2023-01-24 14:16:30 -0300

    Include logs

--- src/logger.ts
+++ src/logger.ts
@@ -1,19 +1,19 @@
-import winston from 'winston'
+// import winston from 'winston'
 
-const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp({ format: 'DD/MM/YYYY HH:mm:ss' }),
-    winston.format.json()
-  ),
-  transports: [
-    new winston.transports.File({
-      filename: '/var/task/src/logs/app.log',
-    }),
-    new winston.transports.File({
-      filename: '/var/task/src/logs/error.log',
-      level: 'error',
-    }),
-  ],
-})
+// const logger = winston.createLogger({
+//   format: winston.format.combine(
+//     winston.format.timestamp({ format: 'DD/MM/YYYY HH:mm:ss' }),
+//     winston.format.json()
+//   ),
+//   transports: [
+//     new winston.transports.File({
+//       filename: '/var/task/src/logs/app.log',
+//     }),
+//     new winston.transports.File({
+//       filename: '/var/task/src/logs/error.log',
+//       level: 'error',
+//     }),
+//   ],
+// })
 
-export { logger }
+// export { logger }
